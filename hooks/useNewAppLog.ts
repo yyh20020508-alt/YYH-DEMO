1a4b90c7c54a42a3650e4b28ae5f8cd0ca84a8a1
yyh20020508-alt <yyh20020508@gmail.com>
2026-06-10 15:52:27 +0800

    Delete hooks directory

--- hooks/useNewAppLog.ts
+++
@@ -1,8 +0,0 @@
-export function useNewAppLog(_opts?: { pageName?: string; agentId?: string; agentName?: string }) {
-    return {
-        log: (..._args: any[]) => {},
-        yaLog: () => {},
-        setPage: (..._args: any[]) => {},
-        source: '',
-    };
-}
