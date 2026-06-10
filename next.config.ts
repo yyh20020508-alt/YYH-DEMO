e4a2aa8cdedfbaf69de1756096dd98eff542d1c4
yyh20020508-alt <yyh20020508@gmail.com>
2026-06-10 16:12:19 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ["172.26.11.166"],
   turbopack: {
     root: __dirname,
   },
