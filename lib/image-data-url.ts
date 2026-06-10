47e1bb5dd5990a0713ed39111b22d650c554805b
yyh20020508-alt <yyh20020508@gmail.com>
2026-06-10 15:52:58 +0800

    Delete lib directory

--- lib/image-data-url.ts
+++
@@ -1,8 +0,0 @@
-export function isInEmbeddedWebView() { return false; }
-export function toAbsoluteImageUrl(url: string) { return url; }
-export function downloadDataUrl(dataUrl: string, filename: string) {
-    const a = document.createElement('a');
-    a.href = dataUrl;
-    a.download = filename;
-    a.click();
-}
