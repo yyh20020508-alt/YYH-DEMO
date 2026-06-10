cb6af10b0199e5035812df8ed36a35e275363e72
yyh20020508-alt <yyh20020508@gmail.com>
2026-06-10 15:51:31 +0800

    Delete components directory

--- components/NewAppFooterButton.tsx
+++
@@ -1,7 +0,0 @@
-export function NewAppFooterButton(_props?: {
-    sceneName?: string;
-    actKey?: string;
-    invokeUrl?: string;
-    btnText?: string;
-    agentName?: string;
-}) { return null; }
--- components/NewAppTopBar.tsx
+++
@@ -1,6 +0,0 @@
-export function NewAppTopBar(_props?: {
-    title?: string;
-    showShare?: boolean;
-    shareData?: any;
-    agentName?: string;
-}) { return null; }
